465b249c3577b968c823bfed8558a59ee10226a8
profUmer <umer.noor@humber.ca>
2023-06-13 15:23:45 -0400

    Adding const keyword to getPos

--- MathLibrary/Ray.h
+++ MathLibrary/Ray.h
@@ -10,7 +10,7 @@ namespace MATHEX{
         Ray(const Vec3 &start_, const Vec3 &direction_): 
             start(start_), direction(direction_){}
 
-        Vec3 getPos(const float t) { 
+        const Vec3 getPos(const float t) const {
             return start + (t * (direction - start));
         }
     };
