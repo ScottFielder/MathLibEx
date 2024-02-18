f47549a59f1233a5b3c17e790d7c53beadbf0c6e
ProfUmer <umer.noor@humber.ca>
2024-02-18 13:58:11 -0500

    Added example of joining points

--- MathLibrary/Join.h
+++ MathLibrary/Join.h
@@ -7,12 +7,13 @@
 #include "Meet.h"
 namespace MATHEX {
 
-	/// The join represents when you "join" two things together. Get it?
-	/// For example if you "join" two point, it gives you the line that goes through them
-	/// More thoughts at the end of this file
+	// The join represents when you "join" two things together. Get it?
+	// For example if you "join" two point, it gives you the line that goes through them
+	// More thoughts at the end of this file
 	
-	/// Two points join to make a line
-	/// Look how easy that is now that we have the Poincare Duality (!) and the Meet (^)
+	// Two points join to make a line
+	// Look how easy that is now that we have the Poincare Duality (!) and the Meet (^)
+	// EXAMPLE: https://github.com/ScottFielder/MathLibrary/blob/master/Notes/Example_joining_points.pdf
 	inline const DualQuat operator & (const MATH::Vec4& p1, const MATH::Vec4& p2) {
 		return !(!p1 ^ !p2);
 	}
@@ -29,8 +30,8 @@ namespace MATHEX {
 }
 #endif
 
-/// Joining two points is handy as it is difficult to see how dual quaternions relate to lines
-/// It's written as an upside down "^" in  math, but unfortunately that is not available on my keyboard
-/// Consensus seems to be to go with overloading "&". I guess because you are joining this "AND" that. Get it?
-/// The join is also known as the regressive product
-/// I like to think of it as a complement to the meet. That's why there are so many duals.
+// Joining two points is handy as it is difficult to see how dual quaternions relate to lines
+// It's written as an upside down "^" in  math, but unfortunately that is not available on my keyboard
+// Consensus seems to be to go with overloading "&". I guess because you are joining this "AND" that. Get it?
+// The join is also known as the regressive product
+// I like to think of it as a complement to the meet. That's why there are so many duals.
