648314c7c670238996134ddb75e6c214776a473d
ProfUmer <umer.noor@humber.ca>
2024-04-30 09:16:57 -0400

    Thinking more about the dot and project

--- MathLibrary/Dot.h
+++ MathLibrary/Dot.h
@@ -6,13 +6,16 @@ namespace MATHEX {
 
 	// Ahh, our old friend the dot product. It still means the same thing in geometric algebra plus some extra superpowers
 	// The consensus in the GA community is to overload the pipe "|" operator for the dot product, so I'll go with that
-	// https://bivector.net/tools.html?p=3&q=0&r=1
+	// I'll use the formula A dot B = <AB>_g, where g is the absolute difference between the grades of A and B
+	// As shown in Hamish Todd's lecture around the 58 min mark: https://www.gdcvault.com/play/1029237/
 	// WHY? https://github.com/ScottFielder/MathLibrary/blob/master/Notes/Why_combine_things.pdf
 	// More thoughts at the end of this file
 
 	// A plane and a line dot to make another plane
 	// This new plane is orthogonal to the original plane and through the line
+	// TODO (UN): Do I need to code up the opposite order for the dot product? Does it matter?
 	inline const MATH::Plane dot(const MATH::Plane& p, const DualQuat& line) {
+		// Extract grade 1 part of the geometric product
 		return (p * line).plane;
 	}
 	inline const MATH::Plane operator | (const MATH::Plane& p, const DualQuat& line) {
@@ -22,7 +25,8 @@ namespace MATHEX {
 	// A plane and a point dot to make a line
 	// This new line is orthogonal to the original plane and through the point!
 	inline const DualQuat dot(const MATH::Plane& p, const MATH::Vec4& v) {
-		return p * MATH::VMath::inverse(v);	
+		// Extract grade 2 part of the geometric product
+		return DQMath::extractLine(p * v);	
 	}
 	inline const DualQuat operator | (const MATH::Plane& p, const MATH::Vec4& v) {
 		return dot(p, v);
@@ -31,12 +35,12 @@ namespace MATHEX {
 	// A line and a point dot to make a plane
 	// This new plane is orthogonal to the original line and through the point!
 	inline const MATH::Plane dot (const DualQuat& line, const MATH::Vec4& v) {
+		// Extract grade 1 part of the geometric product
 		return (line * v).plane;
 	}
 	inline const MATH::Plane operator | (const DualQuat& line, const MATH::Vec4& v){
 		return dot (line, v);
 	}
-
 
 	// Now we are dotting the same type of things together, it returns just a float
 	inline float dot(const MATH::Plane& p1, const MATH::Plane& p2) {
@@ -46,7 +50,6 @@ namespace MATHEX {
 		return dot(p1, p2);
 	}
 
-
 	inline float dot(const DualQuat& line1, const DualQuat& line2) {
 		return (line1 * line2).real;
 	}
@@ -54,12 +57,22 @@ namespace MATHEX {
 		return dot(line1, line2);
 	}
 
-
 	inline float dot(const MATH::Vec4& v1, const MATH::Vec4& v2) {
 		return (v1 * v2).real;
 	}
 	inline float operator | (const MATH::Vec4& v1, const MATH::Vec4& v2) {
 		return dot(v1, v2);
+	}
+
+	// Now that we have the dots, we can project one geometric object onto another
+	// Using the equations from the 58:51 min mark: https://www.gdcvault.com/play/1029237/
+	inline const MATH::Vec4 project(const MATH::Vec4& v, const MATH::Plane& p) {
+		// I adapted the formula a little for two reasons:
+		// 1. I have a plane dot Vec4 function, but not the other way around
+		// 2. I have a DualQuat meet plane function, but not a geometric product function
+		// TODO (UN): Does it matter I coded it up a different way? In my mind, it's the same thing as we are seeing where
+		// the line that goes through the point and is orthogonal to the plane intersects the plane
+		return (p | v) ^ p;
 	}
 }
 #endif
